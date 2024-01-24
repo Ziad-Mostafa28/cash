72c9f649a554eaed4e59437d8c8f19a5828ff1e8
Ziad-Mostafa28 <ziad46363@gmail.com>
2024-01-24 15:20:38 +0200

    style and functions

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,5 @@
 // script.js
+
 const inputs = document.getElementById("inputs");
 
 inputs.addEventListener("input", function (e) {
@@ -67,6 +68,28 @@ inputs.addEventListener("keyup", function (e) {
 });
 
 
-function showPassword() {
+let timerOn = true;
 
+function timer(remaining) {
+  var m = Math.floor(remaining / 60);
+  var s = remaining % 60;
+  
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+  document.getElementById('timer').innerHTML = m + ':' + s;
+  remaining -= 1;
+  
+  if(remaining >= 0 && timerOn) {
+    setTimeout(function() {
+        timer(remaining);
+    }, 1000);
+    return;
+  }
+
+  if(!timerOn) {
+    // Do validate stuff here
+    return;
+  }
 }
+
+timer(120);
